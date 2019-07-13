8793b5936699444c617338ab535764e827b0e170
KavinderSingh <kavindersingh42@gmail.com>
2019-07-13 14:27:03 +0530

    solved SPOJ half of the half

---
+++ spoj.strhh/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        
+        for(int i=0;i< s.length()/2; i+=2)
+        {
+            cout << s[i];
+        }
+        cout << endl;
+
+    }
+
+    return 0;
+}
